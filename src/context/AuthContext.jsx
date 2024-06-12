b1d0e819f60d42ecd789931df229cef9b5cd08a4
Simeon <simeonpopov94@gmail.com>
2024-06-13 00:46:01 +0300

    Refactored to be cleaner the auth context

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,10 +1,8 @@
 import { createContext, useReducer, useEffect } from "react";
 
 // creates context that will be passed along the components
-export const AuthContext = createContext();
+export const AuthContext = createContext(null);
 
-// this function will be used to change the state and
-// we have LOGIN, LOGOUT options for changing the state
 // ! THE 'state' is the previous state
 export const authReducer = (state, action) => {
     switch (action.type){
@@ -17,23 +15,19 @@ export const authReducer = (state, action) => {
     }
 }
 
-//This will generate a component that wraps around the children
-// the children are components and can be multiple
-// all children will have this context and shared state of the user
 export const AuthContextProvider = ({children}) => {
-    //The initial state will be set to null if the user is not present in the localstorage
-    const [state, dispatch] = useReducer(authReducer, {
-        user: JSON.parse(localStorage.getItem('user'))
-    })
+    const initialState = {user: JSON.parse(localStorage.getItem('user'))};
+    const [state, dispatch] = useReducer(authReducer, initialState)
+    const isAuthenticated = !!state.user;
 
-    //This will check for a valid token in the local storage
-    //if the token is valid then the user is logged in
-    //if the token is not valid then the user is not logged in and we remove the invalid token
+    useEffect(() =>{
+        checkIfUserIsLoggedIn();
+    }, [])
     const checkIfUserIsLoggedIn = () => {
         try{
-            let userInfo = JSON.parse(localStorage.getItem('user'));
-            if(userInfo && userInfo.access_token && userInfo.refresh_token){
-                dispatch({type: 'LOGIN', payload: userInfo});
+            let user = JSON.parse(localStorage.getItem('user'));
+            if(user && user.access_token && user.refresh_token){
+                dispatch({type: 'LOGIN', payload: user});
             }
 
         }catch(e){
@@ -43,18 +37,6 @@ export const AuthContextProvider = ({children}) => {
     }
 
 
-    //when the app first loads this will check if the token is set already
-    //this means that the user is logged in by now
-    useEffect(() =>{
-        checkIfUserIsLoggedIn();
-    }, [])
-
-    // Determine if the user is authenticated based on the presence of the user object
-    const isAuthenticated = !!state.user;
-
-    // console.log('AuthContext state: ', state);
-
-    //...state is the context that we will share to the children components
     return (
         <AuthContext.Provider value={{...state, dispatch, isAuthenticated}}>
             { children }
